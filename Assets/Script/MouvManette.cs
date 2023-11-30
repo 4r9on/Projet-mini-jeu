99ab990f2d3836ff5f5ec0cac87e8d1d385ed333
titouan garnier <garniertitouan4@gmail.com>
2023-11-30 09:39:21 +0100

    changement de version

--- Assets/Script/MouvManette.cs
+++ Assets/Script/MouvManette.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MouvManette : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody rb;
+    public float speed = 5.0f;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float AxisX = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * AxisX * Time.deltaTime);
+
+        float AxisY = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speed * AxisY * Time.deltaTime);
     }
 }
--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI timerText;
+    float time;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        time = Time.deltaTime;
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+
     }
 }
