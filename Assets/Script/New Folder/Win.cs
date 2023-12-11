96a1bdb153375c84ee922198b6282c163b498dcf
titouan garnier <garniertitouan4@gmail.com>
2023-12-11 10:10:42 +0100

    Menu

--- Assets/Script/New Folder/Win.cs
+++ Assets/Script/New Folder/Win.cs
@@ -15,11 +15,13 @@ public class Win : MonoBehaviour
 
     public Leaderboard leaderboard;
 
+    public string SceneName;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player1")
         {
-            SceneManager.LoadScene("Board", LoadSceneMode.Additive);
+            SceneManager.LoadScene(SceneName);
 
             winner.text = "Player 1";
            // leaderboard.score = 5;
@@ -28,7 +30,8 @@ public class Win : MonoBehaviour
         }
         if (collision.gameObject.tag == "Player2")
         {
-            SceneManager.LoadScene("Board", LoadSceneMode.Additive);
+            SceneManager.LoadScene(SceneName);
+
 
             winner.text = "Player 2";
             //leaderboard.score = 5;
@@ -37,15 +40,17 @@ public class Win : MonoBehaviour
         }
         if (collision.gameObject.tag == "Player3")
         {
-            SceneManager.LoadScene("Board", LoadSceneMode.Additive);
+            SceneManager.LoadScene(SceneName);
+
 
             winner.text = "Player 3";
-//leaderboard.score = 5;
+            //leaderboard.score = 5;
 
         }
         if (collision.gameObject.tag == "Player4")
         {
-            SceneManager.LoadScene("Board", LoadSceneMode.Additive);
+            SceneManager.LoadScene(SceneName);
+
 
             winner.text = "Player 4";
             //leaderboard.score = 5;
