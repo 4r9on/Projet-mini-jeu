a3a293b33058077f1627fbac26ec45666df400f8
Bryan Berge <berge.bryan@gmail.com>
2023-12-11 13:02:50 +0100

    VsemiFinal

--- Assets/Script/Bryan/Score.cs
+++ Assets/Script/Bryan/Score.cs
@@ -1,31 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     // Liste des objets joueurs
-    public List<GemCollect> playersScore = new List<GemCollect>();
+    public List<GemCollect> players = new List<GemCollect>();
+    public TMP_Text textWin;
 
     void Start()
     {
+        // Appeler la fonction pour trouver le score le plus élevé
         int highestScore = FindHighestScore();
+        textWin.text = "Le joueur à gagné avec " + highestScore + " points";
+
     }
 
     int FindHighestScore()
     {
-        int highestScore = 0;
+        // Si la liste est vide, retourner 0
+        if (players.Count == 0)
+        {
+            return 0;
+        }
+
+        // Initialiser la variable pour stocker le score le plus élevé
+        int highestScore = players[0].score;
 
         // Parcourir tous les joueurs pour trouver le score le plus élevé
-        foreach (GemCollect player in playersScore)
+        for (int i = 1; i < players.Count; i++)
         {
             // Vérifier si le joueur a un score plus élevé que le score actuel
-            if (player.score > highestScore)
+            if (players[i].score > highestScore)
             {
-                highestScore = player.score;
+                highestScore = players[i].score;
             }
         }
 
+        // Retourner le score le plus élevé
         return highestScore;
     }
 }
